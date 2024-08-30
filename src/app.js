349644fc7976eba4fe99f834dd5244af317c4f2c
Eliabr Rodrigues <eliabe859@gmail.com>
2024-08-30 18:40:49 -0300

    criado a rota get

--- src/app.js
+++ src/app.js
@@ -2,9 +2,38 @@ import express from 'express'
 const app = express()
 
 
+const presentes = [
+    {
+        'id':1,
+        'nomePresente': 'Liquidificador',
+        'price': 15,
+        'disponivel': true,
+        'telefoneDoUser': '11951285687'
+    },
+    {
+        'id':2,
+        'nomePresente': 'Batedeira',
+        'price': 20,
+        'disponivel': true,
+        'telefoneDoUser': '11951285687'
+    },
+    {
+        'id':3,
+        'nomePresente': 'Maquina de Lavar',
+        'price': 50,
+        'disponivel': true,
+        'telefoneDoUser': '11951285687'
+    }
+]
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/presentes', (req, res) => {
+    res.send(presentes)
+  })
 
 export default app
 
