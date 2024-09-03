78f1eb6c75b327fbec7b9d26446e2de079a5db9f
Eliabr Rodrigues <eliabe859@gmail.com>
2024-09-03 00:07:13 -0300

    finalizado o put

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express()
+app.use(express.json()) 
+
 
 app.use(cors())
 
@@ -13,26 +15,30 @@ const presentes = [
         'nomePresente': 'Liquidificador',
         'price': 15,
         'disponivel': true,
-        'telefoneDoUser': '11951285687'
+        'telefoneDoUser': "null"
     },
     {
         'id':2,
         'nomePresente': 'Batedeira',
         'price': 20,
         'disponivel': true,
-        'telefoneDoUser': '11951285687'
+        'telefoneDoUser': 'null'
     },
     {
         'id':3,
         'nomePresente': 'Maquina de Lavar',
         'price': 50,
         'disponivel': true,
-        'telefoneDoUser': '11951285687'
+        'telefoneDoUser': 'null'
     }
 ]
 
 function buscarPresentePorId(id){
     return presentes.filter( presente => presente.id == id )
+}
+
+function buscarIndexPorId(id){
+    return presentes.findIndex(presente => presente.id == id)
 }
 
 
@@ -48,6 +54,28 @@ app.get('/presentes', (req, res) => {
 app.get('/presentes/:id', (req, res) => {
     res.json(buscarPresentePorId(req.params.id))
 })
+
+app.post('/presentes', (req, res) => {
+    presentes.push(req.body)
+    res.status(200).send('Mensagem de Confirmação')
+})
+
+app.delete('/presentes/:id', (req, res) => {
+    let index = buscarIndexPorId(req.params.id)
+    presentes.splice(index, 1)
+    res.status(201).send('excluido com sucesso')
+})
+
+app.put('/presentes/:id', (req, res) => {
+    let index = buscarIndexPorId(req.params.id)
+
+    presentes[index].telefoneDoUser = req.body.telefoneDoUser
+    res.json(presentes)
+})
+
+
+
+
 
 export default app
 
