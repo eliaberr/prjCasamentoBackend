ced3e7f45133fcc8166ff5b2f45c0224f39c0371
Eliabr Rodrigues <eliabe859@gmail.com>
2025-11-10 01:39:28 -0300

    fix:fixed the gub deploy at port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 import app from "./app.js"
 
-const port = process.env.PORT || 3001
+const port = 3001
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
