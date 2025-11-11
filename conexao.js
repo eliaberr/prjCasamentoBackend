a999938b3eee7ac712874bb047829134204e1899
Eliabr Rodrigues <eliabe859@gmail.com>
2025-11-11 12:24:47 -0300

    fix: fixed deploy vercel

--- conexao.js
+++ conexao.js
@@ -1,8 +1,8 @@
 import mysql from "mysql2";
 
 const conexao = mysql.createConnection({
-  host: "mysql.railway.internal",
-  port: 3306,
+  host: "gondola.proxy.rlwy.net",
+  port: 25117,
   user: "root",
   password: "nWByHrURQbxVFoGtAYXXMMnJisiRtdJQ",
   database: "presentes"
